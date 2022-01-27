fc344f4f7ed7d53b45c3d2a0925b9443eb34b46a
Matheus Alves Pereira <67547806+matheusgit1@users.noreply.github.com>
2022-01-27 13:33:17 -0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,13 +5,13 @@ import "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBAYqhN9sgq2TnVgPHUFx884ZMMl7BcfaM",
-  authDomain: "wpp-clone-25331.firebaseapp.com",
-  projectId: "wpp-clone-25331",
-  storageBucket: "wpp-clone-25331.appspot.com",
-  messagingSenderId: "237668288756",
-  appId: "1:237668288756:web:63a838cba39867ed8836a1",
-  measurementId: "G-G0F42XXYYX"
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId
 };
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
